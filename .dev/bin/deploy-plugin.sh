35bfd1fd488f965ac4ddcdf1f27109d0933a99ee
JR Tashjian <jrtashjian@gmail.com>
2019-10-06 02:29:36 -0400

    Updating deploy-plugin script

--- .dev/bin/deploy-plugin.sh
+++ .dev/bin/deploy-plugin.sh
@@ -5,19 +5,20 @@ VERSION=$1
 
 WORKING_DIR=`pwd`
 
-svn co "http://svn.wp-plugins.org/${PLUGIN}" $HOME/$PLUGIN
+mkdir -p release/svn
+svn co "http://svn.wp-plugins.org/${PLUGIN}" release/svn
 
-rm -rf $HOME/$PLUGIN/trunk/*
-rsync -av --progress ./* $HOME/$PLUGIN/trunk --exclude node_modules --exclude .dev
+rm -rf release/svn/trunk/*
+rsync -av --progress release/$PLUGIN/* release/svn/trunk
 
-cd $HOME/$PLUGIN
+cd release/svn
+svn status | grep '^!' | awk '{print $2}' | xargs svn delete
 svn add * --force
 svn commit -m "Pushing ${VERSION}"
 
 svn cp trunk tags/$VERSION
 svn commit -m "Taggin version ${VERSION}"
 
-cd $HOME
-wget https://downloads.wordpress.org/plugin/$PLUGIN.$VERSION.zip
-
 cd $WORKING_DIR
+rm -rf release
+echo "https://downloads.wordpress.org/plugin/${PLUGIN}.${VERSION}.zip"
