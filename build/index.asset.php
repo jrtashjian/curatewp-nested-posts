35c76a51e30fc103b43b14e4f9b74cb065155915
JR Tashjian <jrtashjian@gmail.com>
2019-10-05 22:25:10 -0400

    Untrack build files

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7489248e7fbeab9f93af1f07c3ddd9bb');
